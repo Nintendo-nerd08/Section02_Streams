68deaa44c89a45b272ebe6625db2ea3c81efbafc
lloydkev000 <lloydkevin631@gmail.com>
2025-02-26 13:51:02 -0500

    Done

--- src/Filter.java
+++ src/Filter.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class Fliter {
+public class Filter {
+    public static void main(String[] args) {
+       var evens = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        .filter(t -> t % 2 != 0)
+        .toList();
+
+
+
+    }
 }
